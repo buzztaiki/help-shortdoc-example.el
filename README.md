b86bd1fc511b9c24f25ea725ff7a9e36fc6a2b20
Taiki Sugawara <buzz.taiki@gmail.com>
2022-06-10 00:52:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# help-shortdoc
+
+This package allows to output shortdoc examples to `*Help*` buffer.
+
+## Usage
+
+Add the following to your init file:
+
+```lisp
+(help-shortdoc-setup)
+```
+
+For example, if you type `M-x describe-function car`, the following will be output:
+
+```
+car is a built-in function in ‘src/data.c’.
+
+(car LIST)
+
+Return the car of LIST.  If LIST is nil, return nil.
+Error if LIST is not nil and not a cons cell.  See also ‘car-safe’.
+
+See Info node ‘(elisp)Cons Cells’ for a discussion of related basic
+Lisp concepts such as car, cdr, cons cell and list.
+
+  Other relevant functions are documented in the list group.
+  Probably introduced at or before Emacs version 1.2.
+  This function does not change global state, including the match data.
+
+Examples:
+
+  (car '(one two three))
+    ⇒ one
+  (car '(one . two))
+    ⇒ one
+  (car nil)
+    ⇒ nil
+
+[back]
+```
+
+## License
+
+GPLv3
---
+++ help-shortdoc.el
@@ -0,0 +1,57 @@
+;;; help-shortdoc.el --- Output shortdoc examples to *Help* buffer -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Taiki Sugawara
+
+;; Author: Taiki Sugawara <buzz.taiki@gmail.com>
+;; URL: https://github.com/buzztaiki/help-shortdoc.el
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28.1"))
+;; Keywords: help
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package allows to output shortdoc examples to *Help* buffer.
+;; To use it, add the following to your init file:
+;;
+;;         (help-shortdoc-setup)
+
+;;; Code:
+
+(require 'shortdoc)
+(require 'help-fns)
+
+;;;###autoload
+(defun help-shortdoc (function)
+  "Insert shortdoc examples of FUNCTION."
+  (when-let ((groups (and (symbolp function) (shortdoc-function-groups function))))
+    (insert "\n" (propertize "Examples: " 'face 'bold) "\n\n ")
+    (dolist (group groups)
+      (save-restriction
+        (narrow-to-region (point) (point))
+        (shortdoc--display-function (assq function (cdr (assq group shortdoc--groups))))
+        (save-excursion
+          (goto-char (point-min))
+          (when-let (match (text-property-search-forward 'face 'shortdoc-section t t))
+            (delete-region (point-min) (1+ (prop-match-end match))))))
+      (insert "\n"))))
+
+;;;###autoload
+(defun help-shortdoc-setup ()
+  "Setup `help-shortdoc'."
+  (add-hook 'help-fns-describe-function-functions #'help-shortdoc 100))
+
+(provide 'help-shortdoc)
+;;; help-shortdoc.el ends here
